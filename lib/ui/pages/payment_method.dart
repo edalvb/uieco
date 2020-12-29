ec365885d678c0ae8460158ddadc07331eb6c8e8
Edward Alexis <edalvb@gmail.com>
2020-12-29 11:42:08 -0500

    Métodos de pago 2 de 4

--- lib/ui/pages/payment_method.dart
+++ lib/ui/pages/payment_method.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class PaymentMethodPage extends StatefulWidget {
   @override
@@ -6,6 +7,8 @@ class PaymentMethodPage extends StatefulWidget {
 }
 
 class _PaymentMethodPageState extends State<PaymentMethodPage> {
+  var paymentSelected = PaymentMethod.CREDIT_CARD;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -34,9 +37,89 @@ class _PaymentMethodPageState extends State<PaymentMethodPage> {
                 color: Color(0xff6C738A),
               ),
             ),
+            SizedBox(
+              height: 20,
+            ),
+            PaymentMethodWidget(
+              title: "Tarjeta de crédito",
+              subTitle: "Pague con MasterCard o Visa",
+              value: PaymentMethod.CREDIT_CARD,
+              selectedValue: this.paymentSelected,
+              paymentCallback: (value) {
+                setState(() {
+                  this.paymentSelected = value;
+                });
+              },
+            ),
+            PaymentMethodWidget(
+              title: "Transferencia bancaria",
+              subTitle: "Pague directo desde su cuenta",
+              value: PaymentMethod.BANK,
+              selectedValue: this.paymentSelected,
+              paymentCallback: (value) {
+                setState(() {
+                  this.paymentSelected = value;
+                });
+              },
+            ),
           ],
         ),
       ),
     );
+  }
+}
+
+enum PaymentMethod { CREDIT_CARD, BANK, PAYPAL, BITCOIN }
+
+typedef PaymentCallback = Function(PaymentMethod value);
+
+class PaymentMethodWidget extends StatelessWidget {
+  Widget icon;
+  String title;
+  String subTitle;
+  PaymentMethod value;
+  PaymentMethod selectedValue;
+  PaymentCallback paymentCallback;
+
+  PaymentMethodWidget(
+      {this.icon, this.title, this.subTitle, this.value, this.selectedValue, this.paymentCallback});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: _onTap,
+      child: Row(
+        children: <Widget>[
+          getWidgetState(),
+        ],
+      ),
+    );
+  }
+
+  Widget getWidgetState() {
+    if (value == selectedValue) {
+      return Icon(
+        FontAwesomeIcons.solidCheckCircle,
+        color: Colors.green,
+        size: 21,
+      );
+    } else {
+      return Container(
+        margin: EdgeInsets.only(left: 1),
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+          border: Border.all(
+            color: Colors.grey,
+            width: 2,
+          ),
+        ),
+        height: 20,
+        width: 20,
+      );
+    }
+  }
+
+  void _onTap() {
+    this.paymentCallback(this.value);
   }
 }
