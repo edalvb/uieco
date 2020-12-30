fb27d5c5cb4d110bc0bacf58b2539c961e05b2f5
Edward Alexis <edalvb@gmail.com>
2020-12-29 19:40:11 -0500

    Detalles de pago 2 de 3

--- lib/ui/pages/payment_details.dart
+++ lib/ui/pages/payment_details.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:masked_text_input_formatter/masked_text_input_formatter.dart';
 
 class PaymentDetails extends StatefulWidget {
   @override
@@ -8,6 +9,16 @@ class PaymentDetails extends StatefulWidget {
 
 class _PaymentDetailsState extends State<PaymentDetails> {
   final formKey = GlobalKey<FormState>();
+  String name = "";
+  String cardNumber = "";
+  String expiryDate = "";
+  String cvvCode = "";
+
+  var _saveCard = false;
+
+  FocusNode cardNumberFocus;
+  FocusNode expiryDateFocus;
+  FocusNode cvvCodeFocus;
 
   @override
   Widget build(BuildContext context) {
@@ -52,16 +63,68 @@ class _PaymentDetailsState extends State<PaymentDetails> {
               Text(
                 "Ingrese los datos de su tarjeta de crédito",
                 style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 15,
-                  color: Colors.grey
+                    fontWeight: FontWeight.w600,
+                    fontSize: 15,
+                    color: Colors.grey),
+              ),
+              SizedBox(height: 20),
+              TextFormField(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
                 ),
+                decoration: CardInputDecoration("Nombre"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Llene este campo";
+                  }
+                },
+                onSaved: (value) => name = value,
+                onEditingComplete: () => _requestFocus(this.cardNumberFocus),
+              ),
+              TextFormField(
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 12,
+                ),
+                decoration: CardInputDecoration("Número de tarjeta"),
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return "Llene este campo";
+                  }
+                },
+                onSaved: (value) => cardNumber = value,
+                inputFormatters: [
+                  MaskedTextInputFormatter(
+                      mask: "xxxx-xxxx-xxxx-xxxx", separator: "-")
+                ],
+                keyboardType: TextInputType.number,
+                focusNode: cardNumberFocus,
+                textInputAction: TextInputAction.next,
               ),
             ],
           ),
         ),
       ),
     );
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    this.cardNumberFocus = FocusNode();
+    this.expiryDateFocus = FocusNode();
+    this.cvvCodeFocus = FocusNode();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    this.cardNumberFocus.dispose();
+    this.expiryDateFocus.dispose();
+    this.cvvCodeFocus.dispose();
+    super.dispose();
   }
 
   Widget lineSpacer(Color color) {
@@ -75,4 +138,21 @@ class _PaymentDetailsState extends State<PaymentDetails> {
       ),
     );
   }
+
+  _requestFocus(FocusNode focusNode) =>
+      FocusScope.of(context).requestFocus(focusNode);
+}
+
+class CardInputDecoration extends InputDecoration {
+  CardInputDecoration(String myLabelText)
+      : super(
+          fillColor: Color(0xFFF5F5F5),
+          filled: true,
+          hintText: myLabelText,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(25),
+            borderSide: BorderSide.none,
+          ),
+          contentPadding: EdgeInsets.all(10),
+        );
 }
--- lib/ui/pages/payment_method.dart
+++ lib/ui/pages/payment_method.dart
@@ -20,71 +20,75 @@ class _PaymentMethodPageState extends State<PaymentMethodPage> {
         ),
       ),
       body: Padding(
-        padding: const EdgeInsets.only(left: 30, right: 30, top: 10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+        padding: const EdgeInsets.only(left: 30, right: 30, top: 10, bottom: 30),
+        child: ListView(
           children: [
-            Text(
-              "Método de pago",
-              style: TextStyle(
-                fontSize: 18,
-                fontWeight: FontWeight.bold,
-                color: Color(0xff1A1A1A),
-              ),
-            ),
-            Text(
-              "Selecciona uno de los métodos de pago",
-              style: TextStyle(
-                color: Color(0xff6C738A),
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            PaymentMethodWidget(
-              title: "Tarjeta de crédito",
-              subTitle: "Pague con MasterCard o Visa",
-              icon: Icon(
-                FontAwesomeIcons.creditCard,
-                color: Colors.orange,
-              ),
-              value: PaymentMethod.CREDIT_CARD,
-              selectedValue: this.paymentSelected,
-              paymentCallback: _onPaymentChange,
-            ),
-            PaymentMethodWidget(
-              title: "Transferencia bancaria",
-              subTitle: "Pague directo desde su cuenta",
-              icon: Icon(
-                FontAwesomeIcons.university,
-                color: Color(0xFF00A7D9),
-              ),
-              value: PaymentMethod.BANK,
-              selectedValue: this.paymentSelected,
-              paymentCallback: _onPaymentChange,
-            ),
-            PaymentMethodWidget(
-              title: "Paypal",
-              subTitle: "La manera más rápida y segura de enviar dinero",
-              icon: Icon(
-                FontAwesomeIcons.paypal,
-                color: Color(0xFF0D47A1),
-              ),
-              value: PaymentMethod.PAYPAL,
-              selectedValue: this.paymentSelected,
-              paymentCallback: _onPaymentChange,
-            ),
-            PaymentMethodWidget(
-              title: "Billetera de Bitcoin",
-              subTitle: "Enviar la cantidad a nuestra billetera Bitcoin",
-              icon: Icon(
-                FontAwesomeIcons.bitcoin,
-                color: Colors.black,
-              ),
-              value: PaymentMethod.BITCOIN,
-              selectedValue: this.paymentSelected,
-              paymentCallback: _onPaymentChange,
-            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Método de pago",
+                  style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xff1A1A1A),
+                  ),
+                ),
+                Text(
+                  "Selecciona uno de los métodos de pago",
+                  style: TextStyle(
+                    color: Color(0xff6C738A),
+                  ),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                PaymentMethodWidget(
+                  title: "Tarjeta de crédito",
+                  subTitle: "Pague con MasterCard o Visa",
+                  icon: Icon(
+                    FontAwesomeIcons.creditCard,
+                    color: Colors.orange,
+                  ),
+                  value: PaymentMethod.CREDIT_CARD,
+                  selectedValue: this.paymentSelected,
+                  paymentCallback: _onPaymentChange,
+                ),
+                PaymentMethodWidget(
+                  title: "Transferencia bancaria",
+                  subTitle: "Pague directo desde su cuenta",
+                  icon: Icon(
+                    FontAwesomeIcons.university,
+                    color: Color(0xFF00A7D9),
+                  ),
+                  value: PaymentMethod.BANK,
+                  selectedValue: this.paymentSelected,
+                  paymentCallback: _onPaymentChange,
+                ),
+                PaymentMethodWidget(
+                  title: "Paypal",
+                  subTitle: "La manera más rápida y segura de enviar dinero",
+                  icon: Icon(
+                    FontAwesomeIcons.paypal,
+                    color: Color(0xFF0D47A1),
+                  ),
+                  value: PaymentMethod.PAYPAL,
+                  selectedValue: this.paymentSelected,
+                  paymentCallback: _onPaymentChange,
+                ),
+                PaymentMethodWidget(
+                  title: "Billetera de Bitcoin",
+                  subTitle: "Enviar la cantidad a nuestra billetera Bitcoin",
+                  icon: Icon(
+                    FontAwesomeIcons.bitcoin,
+                    color: Colors.black,
+                  ),
+                  value: PaymentMethod.BITCOIN,
+                  selectedValue: this.paymentSelected,
+                  paymentCallback: _onPaymentChange,
+                ),
+              ],
+            )
           ],
         ),
       ),
